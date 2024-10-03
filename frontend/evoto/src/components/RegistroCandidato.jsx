a266f201d590512afedb7417e14c9bd08e923faa
MrSchopenhauer <jose.uniriva15@gmail.com>
2024-10-02 22:47:25 -0600

    Primera prueba de frontend Hola Mundo

---
+++ frontend/evoto/src/components/RegistroCandidato.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const RegistroCandidato = () => {
+    return (
+        <div>
+            Hola Mundo
+        </div>
+    )
+}
+
+export default RegistroCandidato
---
+++ frontend/evoto/src/pages/RegistroCandidatoPag.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RegistroCandidato from '../components/RegistroCandidato'
+
+const RegistroCandidatoPag = () => {
+    return(
+        <div>
+            <RegistroCandidato/>
+        </div>
+    )
+}
+
+export default RegistroCandidatoPag 
