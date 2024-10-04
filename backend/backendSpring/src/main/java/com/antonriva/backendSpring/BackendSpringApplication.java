275859feaa3a63193b4fb53e196439baf5cc72ea
MrSchopenhauer <jose.uniriva15@gmail.com>
2024-10-04 09:08:48 -0600

    Creación de projecto spring-maven y eclipse-workspace

---
+++ backend/backendSpring/src/main/java/com/antonriva/backendSpring/BackendSpringApplication.java
@@ -0,0 +1,16 @@
+package com.antonriva.backendSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringApplication.class, args);
+		
+	
+		
+	}
+
+}
