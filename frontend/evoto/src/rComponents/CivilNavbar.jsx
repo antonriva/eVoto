41bb0e7b26edce06286279581d99d92b58736e91
MrSchopenahuer <jose.uniriva15@gmail.com>
2025-06-20 10:28:51 -0600

    New structure

---
+++ frontend/evoto/src/rComponents/CivilNavbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const CivilNavbar = () => (
+  <nav
+    className="navbar"
+    style={{ backgroundColor: "#336699", color: "white", padding: "0.5rem" }}
+  >
+    <div className="container-fluid">
+      <span className="navbar-text">Civil: Registro y Búsqueda</span>
+    </div>
+  </nav>
+);
+
+export default CivilNavbar;
